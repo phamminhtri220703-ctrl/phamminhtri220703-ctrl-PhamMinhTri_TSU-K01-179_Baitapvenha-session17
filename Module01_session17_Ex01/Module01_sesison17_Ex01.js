ae43f0414889d0cf1f888ce953a32ddd2efd9f28
phamminhtri220703-ctrl <phamminhtri220703@gmail.com>
2026-06-09 01:51:19 +0900

    Module01_sesison17_Ex01

---
+++ Module01_session17_Ex01/Module01_sesison17_Ex01.js
@@ -0,0 +1,12 @@
+
+let text = document.getElementById("text-content");
+
+
+function hideText() {
+    text.style.display = "none";
+}
+
+
+function showText() {
+    text.style.display = "block";
+}
